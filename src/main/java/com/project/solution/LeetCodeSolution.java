941d2129d39720386812a0626e43428b64c95e7a
Emerson Millard <emillard.code@gmail.com>
2022-08-08 12:00:00 -0700

    Added solution class.

--- src/main/java/com/project/solution/LeetCodeSolution.java
+++ src/main/java/com/project/solution/LeetCodeSolution.java
@@ -2,6 +2,28 @@ package com.project.solution;
 
 public class LeetCodeSolution {
 
+    public static void main(String[] args) {
 
+        System.out.println(method(3, 7));
+
+    }
+
+    public static int method(int vertical, int horizontal) {
+
+        if (vertical == 1 && horizontal == 1) {
+            return 1;
+        }
+
+        if (vertical < 0 || horizontal < 0) {
+            return 0;
+        }
+
+        int up = method(vertical - 1, horizontal);
+
+        int right= method(vertical, horizontal - 1);
+
+        return up + right;
+
+    }
 
 }
