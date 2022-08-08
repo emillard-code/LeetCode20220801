66dee4a5e91ec06c0391db7c0ce57b48ad20b8a1
Emerson Millard <emillard.code@gmail.com>
2022-08-08 12:00:00 -0700

    Modified attempt class.

--- src/main/java/com/project/attempt/LeetCodeApp.java
+++ src/main/java/com/project/attempt/LeetCodeApp.java
@@ -1,4 +1,30 @@
 package com.project.attempt;
 
 public class LeetCodeApp {
+
+    private static int unique_paths = 0;
+
+    public static void main(String[] args) {
+
+        method(3, 7);
+        System.out.println(unique_paths);
+
+    }
+
+    public static void method(int vertical, int horizontal) {
+
+        if (horizontal > 1) {
+            method(vertical, horizontal - 1);
+        }
+
+        if (vertical > 1) {
+            method(vertical - 1, horizontal);
+        }
+
+        if (vertical == 1 && horizontal == 1) {
+            unique_paths++;
+        }
+
+    }
+
 }
